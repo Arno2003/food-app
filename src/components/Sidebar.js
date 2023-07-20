dcc8bd4a3d44a2f21da9c1e86d3e5732de73d446
Arno2003 <arnabkarate@gmail.com>
2023-07-20 21:55:44 +0530

    1

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import { FontAwesomeIcon } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fontawesome/@fontawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
 import { faList } from "@fortawesome/free-solid-svg-icons";
@@ -7,7 +7,7 @@ import { icon } from "@fortawesome/fontawesome-svg-core";
 export default function Sidebar({links}){
     return (
         <div className="sidebar">
-            <a href="#!" key="Home"><FontAwesomeIcon icon:faHome />Home</a>
+            <a href="#!" key="Home"><FontAwesomeIcon icon={faHome} />Home</a>
             <a href="#!" key="Recipes">Recipes</a>
             <a href="#!" key="Settings">Settings</a>
         </div>
